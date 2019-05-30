1d64e44180ab224427b683b4e57099da814f91ba
Justin Ripley <lloydripley@gmail.com>
2019-05-30 19:43:55 -0400

    it looks like the indep residual for the scalar field is converging for the scalar field on fixed Mink background

--- examples/massless_scalar_collapse/setup_runs.py
+++ examples/massless_scalar_collapse/setup_runs.py
@@ -51,9 +51,9 @@ run_data = {
 ###
 ###	Nx should be of the form 2**n + 1 with n an integer
 ###
-	"Nx"		: 2**9+1,
-	"Nt"		: 2**11,
-	"t_step_save"	: 2**0,
+	"Nx"		: 2**11+1,
+	"Nt"		: 2**14+1,
+	"t_step_save"	: 2**4,
 	"cfl_num"	: 0.25,  
 	"errlim"	: 1.0e-10, 
 	
