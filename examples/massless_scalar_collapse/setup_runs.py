1615453a1717674303863bf8a63ed7514dd31223
Justin Ripley <lloydripley@gmail.com>
2019-05-30 21:21:03 -0400

    now converging with multiple fixed grids

--- examples/massless_scalar_collapse/setup_runs.py
+++ examples/massless_scalar_collapse/setup_runs.py
@@ -51,8 +51,8 @@ run_data = {
 ###
 ###	Nx should be of the form 2**n + 1 with n an integer
 ###
-	"Nx"		: 2**11+1,
-	"Nt"		: 2**14+1,
+	"Nx"		: 2**9+1,
+	"Nt"		: 2**13+1,
 	"t_step_save"	: 2**4,
 	"cfl_num"	: 0.25,  
 	"errlim"	: 1.0e-10, 
